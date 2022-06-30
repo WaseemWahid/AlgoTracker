4decfeb7132cf5a5b8b3cc8a5cbfca0b6b4f7fac
Waseem <wahid.waseem@gmail.com>
2022-06-30 16:34:29 -0700

    merge sorted array algo

--- Array/Easy/SearchInsertPosition.js
+++ Array/Easy/SearchInsertPosition.js
@@ -52,16 +52,3 @@ console.log(searchInsert([1, 3, 5, 6], 2));
 console.log(searchInsert([2, 5], 1));
 
 
-
-var plusOne = function(digits) {
-    for(let i = digits.length - 1; i >= 0; i--) {
-        digits[i]++
-        if(digits[i] > 9) {
-            digits[i] = 0;
-        } else {
-            return digits
-        }
-    }
-    digits.unshift(1)
-    return digits
-};
