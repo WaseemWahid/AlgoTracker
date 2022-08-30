1cefaf01a8172127c3eb253cec57e4e118b582ac
Waseem <wahid.waseem@gmail.com>
2022-08-29 22:18:07 -0700

    Two sum algo easy

--- Array/Easy/TwoSum.js
+++ Array/Easy/TwoSum.js
@@ -45,6 +45,23 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
 
+const nums1 = [2,7,11,15];
+const target1 = 9
+
+var twoSum = function(nums, target) {
+    solution = []
+    for(i = 0; i < nums.length-1; i++) {
+        for(j = i +1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) {
+                solution.push(i, j)
+                return solution
+            }
+        }
+    }
+    return solution
 };
+
+
+
+console.log(twoSum(nums1, target1))
