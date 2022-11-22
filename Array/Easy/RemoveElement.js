9e346a47f22d641644d2ed291943aea3de02ac5e
Waseem <wahid.waseem@gmail.com>
2022-11-21 20:14:42 -0800

    remove element algorithm

--- Array/Easy/RemoveElement.js
+++ Array/Easy/RemoveElement.js
@@ -55,5 +55,12 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-
+    k = 0;
+    for(i = 0; i < nums.length; i++) {
+        if(nums[i] != val)
+        {
+            nums[k++] = nums[i]
+        }
+    }
+    return k;
 };
